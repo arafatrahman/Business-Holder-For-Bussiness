85adcb2b93833e91bb4c4f029cd8f3f623e8349d
Arafat Rahman <70848865+kauniaweb@users.noreply.github.com>
2020-11-27 17:32:10 +0600

    footer added

--- footer.php
+++ footer.php
@@ -1,14 +1,8 @@
-
-<div class="copyright-wrapper">
-        	<div class="container">
-                <div class="copyright">
-                    	<p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php esc_html(bloginfo( 'name' )); ?>  <?php echo esc_html(date_i18n( __( 'Y', 'flydecor' ) )); ?> <?php esc_html_e('. Powered by WordPress'); ?></p>               
-                </div><!-- copyright --><div class="clear"></div>           
-            </div><!-- container -->
-        </div>
-    </div>
-        
+<footer class="kau-theme-footer ">
+	<p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php esc_html(bloginfo( 'name' )); ?>  <?php echo esc_html(date_i18n( __( 'Y' ) )); ?> <?php esc_html_e('. Powered by WordPress'); ?></p>               
+</footer>
 <?php wp_footer(); ?>
 
 </body>
 </html>
+
